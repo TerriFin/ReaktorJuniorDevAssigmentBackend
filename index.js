e65fddb39627da9703134c12742bd3693c04c00a
TerriFin <saukkonensami@gmail.com>
2019-12-01 19:19:06 +0200

    Added procfile and modified port in preparation for heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = require('./app')
 // Create the server and register our express middleware to handle requests
 const server = http.createServer(app)
 
-const port = 3001
+const port = process.env.PORT || 3001
 
 // Start the server
 server.listen(port, () => {
